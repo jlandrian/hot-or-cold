ae75ca501b6b654cfd6ee92cf46688a0451ba9f2
jlandrian <jlandrian@gmail.com>
2014-04-24 15:07:46 -0700

    Guess number increases. more feedback msgs

--- js/app.js
+++ js/app.js
@@ -1,37 +1,42 @@
-$(document).ready(function(){
-    /*---Function to start a  new game---*/
-    /*---function newGame(){
-        Clone the input field for use later
-        var feedbackClone = $("#feedback").clone();
-        Clone the Count for later use
-        var countClone = $("#count").clone();
-        Clone the list of guesses for later use
-        var guessClone = $("#guessList").clone();
-    }---*/
+    /*---Generate a random number b/t 1 - 100---*/
+    var rando = Math.floor((Math.random()*100)+1);
+    /*---Number of guesses---*/
+    var startingCount = 0;
+    /*---Put focus on input field---*/
+    $("#userGuess").focus();
+    /*---Compare the guess with the randomly generated number---*/
     
-    /*---Run New Game Function
-    newGame();---*/
     
-    /*---Generate a random number---*/
-     var rando = Math.floor((Math.random()*100)+1);
     
     /*---Function to determine guesses---*/
     var theGuess = function(){
-        var myNumber = $("#userGuess").val();    
+        var myNumber = $("#userGuess").val();
+        var countUp = $("#count").text(startingCount += 1);
+        var difference = Math.abs(myNumber - rando);
         if(myNumber === rando) {
             $("#feedback").text("yes");
+        } else if (difference >= 50) {
+            $("#feedback").text("cold...");
+            $("#feedback").css("background-color: #00FFFF");
+        } else if (difference >= 30 && difference < 50) {
+            $("#feedback").text("warming up");
+        } else if (difference >= 20 && difference < 30) {
+            $("#feedback").text("warmer still...");
+        } else if (difference >=10 && difference < 20) {
+            $("feedback").text("so dang hot!!!");
         } else {
-            $("#feedback").text("nope!");
+            $("#feedback").text("cold as hell!!!");
+            countUp;
         }
+        
+        $("#userGuess").focus();
     }
-      
+    
+$(document).ready(function(){    
+    
+    /*---Run guessing function when the button is clicked---*/
     $("#guessButton").on("click", theGuess);
-    
-    
 
-    
-    /*---Run New Game on clicking .new button---*/
-    $(".new").on("click", newGame);
     
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
